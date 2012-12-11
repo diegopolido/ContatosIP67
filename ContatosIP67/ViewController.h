6f165a6d8eca3a2f70d26beab812bd4ecc35532a
ios3135 <ios3135@Mac-mini-de-Caelum-36.local>
2012-12-11 19:14:01 -0200

    First commit

---
+++ ContatosIP67/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios3135 on 11/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
