7ad276d6e3c1e59ad9501b3e86280b7026321c83
ios3135 <ios3135@Mac-mini-de-Caelum-36.local>
2012-12-11 21:48:01 -0200

    Creating Contact model

---
+++ ContatosIP67/Contato.h
@@ -0,0 +1,13 @@
+//
+//  Contato.h
+//  ContatosIP67
+//
+//  Created by ios3135 on 11/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contato : NSObject
+
+@end
---
+++ ContatosIP67/Contato.m
@@ -0,0 +1,13 @@
+//
+//  Contato.m
+//  ContatosIP67
+//
+//  Created by ios3135 on 11/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Contato.h"
+
+@implementation Contato
+
+@end
